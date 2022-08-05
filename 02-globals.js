8df143cd83f63e906a5186f30aa030647a9a0c37
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-05 15:43:13 -0300

    First commit

---
+++ 02-globals.js
@@ -0,0 +1,5 @@
+- __dirname: path to current directory.
+- __filename: file name.
+- require: function to use modules.
+- module: info about current module.
+- process: info about the environment where the program is being executed.
